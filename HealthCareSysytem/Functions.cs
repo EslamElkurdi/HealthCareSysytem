31fcb1dee958d7aeb01fe058374efab7d1390617
ESLAM <i.jamal.ict@btu.edu.eg>
2022-11-14 20:25:40 +0200

    Islam Gamal Create Set and Get data functions

---
+++ HealthCareSysytem/Functions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareSysytem
+{
+    class Functions
+    {
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Eslam\OneDrive\Documents\DatabaseHelthCare.mdf;Integrated Security=True;Connect Timeout=30";
+            con = new SqlConnection(ConStr);
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+        }
+
+        public DataTable GetData(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, con);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(string Query) 
+        {
+            int cnt = 0;
+            if (con.State == ConnectionState.Closed) 
+            {
+                con.Open();
+            }
+            cmd.CommandText = Query;
+            cnt = cmd.ExecuteNonQuery();
+            con.Close();
+            return cnt;
+        }
+    }
+}
